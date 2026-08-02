3e8b3f66f8ea64b7b824481f6fc47516311c7f89
Mariacristina Colombera <mariacristina.colombera@gmail.com>
2026-08-02 16:30:18 +0200

    Initial commit - Eliora begins

---
+++ apps/web/src/App.tsx
@@ -0,0 +1,25 @@
+export default function App() {
+  return (
+    <main
+      style={{
+        minHeight: "100vh",
+        background: "#f6f1e8",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        padding: "2rem",
+      }}
+    >
+      <h1
+        style={{
+          fontSize: "3rem",
+          fontWeight: 300,
+          color: "#2f3a38",
+          letterSpacing: "-0.03em",
+        }}
+      >
+        Eliora
+      </h1>
+    </main>
+  );
+}
